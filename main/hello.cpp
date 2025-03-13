02ad1cbd0912c8117b23f757254a4fca549ac388
jayanth r <jayanthramesh16.jr@gmail.com>
2025-03-13 16:07:01 +0530

    Made changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"Made changes \n";
+  cout<<"Made changes Jayanth(PES1UG22CS253) \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
